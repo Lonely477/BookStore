10e586cb5b6e56f2fe987c118eb17448f3e065d9
filip <128900467+Lonely477@users.noreply.github.com>
2024-06-09 12:54:09 +0200

    Pages cont.

---
+++ BookStore/BookStore.Presentation/Extensions/EnumExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace BookStore.Presentation.Extensions;
+
+public static class EnumExtension
+{
+    public static string GetDescription(this Enum value)
+    {
+        System.Reflection.FieldInfo? field = value.GetType().GetField(value.ToString());
+        if (field is null)
+        {
+            return value.ToString();
+        }
+
+        DescriptionAttribute? attribute = (DescriptionAttribute?)Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute));
+        return attribute?.Description ?? value.ToString();
+    }
+}
